c5be8f966fff86685aa43f4699c82b4465714313
canvrno <46584286+canvrno@users.noreply.github.com>
2025-05-17 14:52:02 -0700

    fetchOpenGraphData protobus migration (#3549)

--- src/shared/WebviewMessage.ts
+++ src/shared/WebviewMessage.ts
@@ -41,7 +41,6 @@ export interface WebviewMessage {
 		| "fetchLatestMcpServersFromHub"
 		| "telemetrySetting"
 		| "openSettings"
-		| "fetchOpenGraphData"
 		| "invoke"
 		| "updateSettings"
 		| "clearAllTaskHistory"
