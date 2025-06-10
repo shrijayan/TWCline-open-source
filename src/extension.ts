f2f39d6f1d714095362adfea6544747528f313a2
celestial-vault <58194240+celestial-vault@users.noreply.github.com>
2025-06-10 09:35:35 -0700

    migrate plan/act state to workspace storage (#4130)

--- src/extension.ts
+++ src/extension.ts
@@ -25,6 +25,7 @@ import { WebviewProviderType as WebviewProviderTypeEnum } from "@shared/proto/ui
 import { WebviewProviderType } from "./shared/webview/types"
 import { sendHistoryButtonClickedEvent } from "./core/controller/ui/subscribeToHistoryButtonClicked"
 import { sendAccountButtonClickedEvent } from "./core/controller/ui/subscribeToAccountButtonClicked"
+import { migratePlanActGlobalToWorkspaceStorage } from "./core/storage/state"
 
 /*
 Built using https://github.com/microsoft/vscode-webview-ui-toolkit
@@ -46,6 +47,9 @@ export async function activate(context: vscode.ExtensionContext) {
 	ErrorService.initialize()
 	Logger.initialize(outputChannel)
 	Logger.log("Cline extension activated")
+
+	// Migrate global storage values to workspace storage (one-time cleanup)
+	await migratePlanActGlobalToWorkspaceStorage(context)
 
 	// Version checking for autoupdate notification
 	const currentVersion = context.extension.packageJSON.version
